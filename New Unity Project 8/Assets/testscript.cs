961c274743bdb350030447a883d4dbbce4de7c08
Collin Bannink <cococol2000@gmail.com>
2018-04-19 12:20:48 +0200

    test project to build for ar testing.

---
+++ New Unity Project 8/Assets/testscript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testscript : MonoBehaviour {
+
+    Rigidbody rigidbody;
+    const float WalkSpeed = 20.0f;
+
+    // Use this for initialization
+    void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 velocity = Vector3.zero;
+        float speed = WalkSpeed;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            velocity.z += speed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            velocity.z -= speed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            velocity.x -= speed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            velocity.x += speed;
+        }
+
+        rigidbody.velocity = transform.TransformVector(velocity);
+
+    }
+}
